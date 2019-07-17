6add9eae76793d9b4965b25850a989081aa8d028
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-07-17 14:04:18 +0200

    Removed apt-vim and updated setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
-# Remove files if they exist
+# Remove soft links for update if they existed already
+
 if [ -f $HOME/.gitconfig ]; then
     rm -rf $HOME/.gitconfig
-fi
-
-if [ -f $HOME/.apt-vim ]; then
-    rm -rf $HOME/.apt-vim
 fi
 
 if [ -f $HOME/.vimrc ]; then
@@ -15,11 +12,11 @@ if [ -f $HOME/.zshrc ]; then
     rm -rf $HOME/.zshrc
 fi
 
-# SOFT LINKS
+# Create all soft links
+
 # Git
 ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
 # Vim
-ln -s $HOME/dotfiles/vim/apt-vim $HOME/.apt-vim
 ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 # Zsh
 ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
