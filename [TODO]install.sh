df7c42f6ec5173149a0f14e0abfb28411e0ecbb8
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-12-31 18:22:04 +0100

    Install script instructions NOT DONE

---
+++ [TODO]install.sh
@@ -0,0 +1,11 @@
+# ssh key git
+# install oh-my-zsh:
+	# sudo apt install curl
+	# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install thefuck:
+	# sudo apt install python3-dev python3-pip python3-setuptools
+	# sudo pip3 install thefuck
+# install nerdtree: 
+	# git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+	# vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q 
+# execute update.sh
