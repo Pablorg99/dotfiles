cf57201804f89d7705d1935e588ff57ccd641e65
Pablo Rodríguez Guillén <i72rogup@uco.es>
2020-01-09 16:22:25 +0100

    Added comments to installation script (TODO)

--- [TODO]install.sh
+++ [TODO]install.sh
@@ -1,10 +1,13 @@
+# install git: sudo apt install git
 # ssh key git
 # install oh-my-zsh:
 	# sudo apt install curl
 	# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#install fira code: sudo apt install fonts-firacode
 # install thefuck:
 	# sudo apt install python3-dev python3-pip python3-setuptools
 	# sudo pip3 install thefuck
+# install vim: sudo apt install vim
 # install nerdtree: 
 	# git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 	# vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q 
