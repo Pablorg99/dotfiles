f0ef3aeb6dcb073f775634b18c26f588d3b592b1
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-01-30 14:57:21 +0100

    Aliases updated

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,18 +1,27 @@
-# Common terminal use
-alias rf='rm -rf'
-alias poweroff='systemctl poweroff -i'
-alias reboot='systemctl reboot -i'
+# TERMINAL USE
+alias rf="rm -rf"
+alias poweroff="systemctl poweroff -i"
+alias reboot="systemctl reboot -i"
 
-# Meta aliases
-alias aliases='vim /home/pablorg99/dotfiles/zsh/custom/aliases.zsh'
-alias dotfiles-update='bash /home/pablorg99/dotfiles/update.sh'
+# META ALIASES
+alias aliases="vim $HOME/dotfiles/zsh/custom/aliases.zsh"
+alias dotfiles-update="source $HOME/dotfiles/update.sh"
 
-# Directories
-alias djangorecipes='cd /home/pablorg99/development/repositories/sideprojects/django-recipes && source venv/bin/activate'
-alias ucop='cd /home/pablorg99/development/repositories/university/UCO-Practices'
+# DIRECTORIES
+alias asl="cd $HOME/development/repositories/asl"
+alias uni="cd $HOME/development/repositories/university"
+alias sp="cd $HOME/development/repositories/sideprojects"
 
-# Others
-alias templatex='cp /home/pablorg99/dotfiles/latex/latexTemplate.tex . && mv latexTemplate.tex'
-alias tskata='cp -r /home/pablorg99/Repos/codekata-template-typescript/. .'
-alias pykata='cp -r /home/pablorg99/Repos/codekata-template-python/. .'
-alias ucossh='ssh i72rogup@ts.uco.es'
+# REPOSITORIES
+alias djangorecipes="cd $HOME/development/repositories/sideprojects/django-recipes && source venv/bin/activate"
+alias reactrecipes="cd $HOME/development/repositories/sideprojects/react-recipes"
+alias ucopuntomobile="cd $HOME/development/repositories/sideprojects/ucopunto-mobile"
+alias practicesuco="cd $HOME/development/repositories/university/UCO-Practices"
+
+# TEMPLATES
+alias templatex="cp $HOME/dotfiles/templates/latexTemplate.tex . && mv latexTemplate.tex"
+alias tskata="cp -r $HOME/dotfiles/templates/codekata-template-typescript/. ."
+alias pykata="cp -r $HOME/dotfiles/templates/codekata-template-python/. ."
+
+# OTHERS
+alias ucossh="ssh i72rogup@ts.uco.es"
