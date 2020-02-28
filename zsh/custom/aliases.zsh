23f065d56848d99847b409c07941a5291d373000
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-02-28 10:08:45 +0100

    Sys uptadte and ec2ssh aliases added

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,5 +1,6 @@
 # TERMINAL USE
 alias rf="rm -rf"
+alias sysupdate="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
 alias poweroff="systemctl poweroff -i"
 alias reboot="systemctl reboot -i"
 
@@ -30,3 +31,4 @@ alias new-venv="virtualenv -p python3 venv && source venv/bin/activate"
 
 # OTHERS
 alias ucossh="ssh -X i72rogup@ts.uco.es"
+ec2ssh() {ssh -X ec2-user@"$1" -i $HOME/MEGA/Universidad/Tercero/Otros/AWS/PrivateKeyEC2.pem;}
