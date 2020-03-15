bc703ac3665ed253cd2cfbd3fbf9d060b7aaeecb
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-03-15 16:57:17 +0100

    Added tilda config

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ ln -sfn $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
 ln -sfn $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
 ln -sfn $HOME/dotfiles/code/settings.json $HOME/.config/Code/User/settings.json
 ln -sfn $HOME/dotfiles/python/.pylintrc $HOME/.pylintrc
+ln -sfn $HOME/dotfiles/tilda/config_0 $HOME/.config/tilda/config_0
 
 # RELOAD SHELL CONFIGURATION
 source $HOME/.zshrc
