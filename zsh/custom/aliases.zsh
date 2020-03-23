91c3fe48ea5719c6d51094a782518695ffcf7924
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-03-23 17:01:41 +0100

    issbc alias added and dotfiles-update removed

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -6,10 +6,6 @@ alias sysupdate="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -
 alias poweroff="systemctl poweroff -i"
 alias reboot="systemctl reboot -i"
 
-# META ALIASES
-alias aliases="vim $HOME/dotfiles/zsh/custom/aliases.zsh"
-alias dotfiles-update="source $HOME/dotfiles/update.sh"
-
 # DIRECTORIES
 alias sp="cd $HOME/development/repositories/sideprojects"
 alias katas="cd $HOME/development/repositories/codekatas"
@@ -18,6 +14,7 @@ alias uni="cd $HOME/development/repositories/university"
 
 # TEMPORAL DIRECTORIES
 alias ir="cd $HOME/MEGA/Universidad/Tercero/Cuatrimestre_2/IR/Teoria/Apuntes/ && code ."
+alias issbc="cd $HOME/development/repositories/university/UCO-Practices/Tercero/ISSBC"
 
 # REPOSITORIES
 alias djangorecipes="cd $HOME/development/repositories/sideprojects/django-recipes && source venv/bin/activate"
@@ -37,5 +34,6 @@ alias new-venv="virtualenv -p python3 venv && source venv/bin/activate"
 # OTHERS
 alias ucossh="ssh -X i72rogup@ts.uco.es"
 alias ec2ssh="connectToEC2InstanceFromIP"
+alias aliases="vim $HOME/dotfiles/zsh/custom/aliases.zsh"
 alias ytdownload="youtube-dl -f best --restrict-filename -o '$HOME/Videos/%(title)s.%(ext)s'"
 alias ytplaylist="youtube-dl -f best --restrict-filename -o '$HOME/Videos/%(playlist_index)s-%(title)s.%(ext)s'"
